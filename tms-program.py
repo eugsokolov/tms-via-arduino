e94ae052c9973e0ba93be5cc8812b99bb63ff2e0
eugene sokolov <eugsokolov@gmail.com>
2016-04-11 23:17:38 -0400

    update

--- tms-program.py
+++ tms-program.py
@@ -1,42 +1,63 @@
 import serial
 import csv
-import time
-import datetime, os, syslog
-#import Image
-import subprocess
 import random
+import time, datetime
+import os, subprocess, syslog
+#import Image
 
 def fire_tms(port):
-#Make inter OS operable
-   port = "/dev/ttyACM0"
-   print "FIRING TMS"
-   arduino = serial.Serial(port, 230400)
-   arduino.write('1')
-   arduino.close()
+	port = "/dev/ttyACM0"
+	print "FIRING TMS : ", port
+	#arduino = serial.Serial(port, 230400)
+	#arduino.write('1')
+	#arduino.close()
+
+def error(message):
+	print message
+	exit()
 
 def process_word(config, i):
-#probably use generators
-   return 0
+###DO probably use generators
+	return 0
 
 def process_picture(config, i):
+   # Get random Image to show from image directory
+   if os.path.isdir(config['directory']):
+	path, dirs, files = os.walk(config['directory']).next()
+	r = random.randrange(1,len(files)+1) 
+	image_file = files[r-1] 
+   else: error("Error in configuration \"directory\", input must be valid directory or file")
+
+### DO get image to show
+#image = Image.open(image_file)
+#image.show()
+#time.sleep(float(duration)/1000)
+
    fire = determine_fire(config['fire iteration'], i)
-   path, dirs, files = os.walk(config['directory']).next()
-   r = random.randrange(1,len(files)) 
-   image_file = files[r] 
-   #image = Image.open(image_file)
-   #image.show()
-   #time.sleep(float(duration)/1000)
- 
-#   p = subprocess.Popen(["display", image_file])
-   if fire is True:
+   if config['TMS before or after'] == "before":
+	if fire is True: fire_tms(config['TMS port'])
 	time.sleep(float(config['time to fire'])/1000)
-	fire_tms(config['TMS port'])
-	remaining = float(int(config['total time']) - int(config['time to fire']))/1000
-	time.sleep(remaining)
-   else:
-	time.sleep(float(config['total time'])/1000)
-#   p.terminate()
-#   p.kill()
+#	p = subprocess.Popen(["display", image_file])
+   elif config['TMS before or after'] == "after":
+#	p = subprocess.Popen(["display", image_file])
+	time.sleep(float(config['time to fire'])/1000)
+	if fire is True: fire_tms(config['TMS port'])
+   else: error("Error in configuration \"TMS before or after\", input must be \"before/after\"")
+
+   if config['event end'] == "keypress":
+	raw_input("waiting for keypress to end...")
+   elif config['event end'] == "time":
+	time.sleep(float(config['event end time'])/1000)
+   else: error("Error in configuration \"event end\", input must be \"keypress/time\"")
+
+   if config['refresh'] == "yes":
+	print "close image"
+#	p.terminate()
+#	p.kill()
+   elif config['refresh'] == "no":
+	print "not sure here" 
+   else: error("Error in configuration \"refresh\", input must be \"yes/no\"")
+
    return fire, image_file
 
 def determine_fire(fireiter, i):
@@ -44,10 +65,10 @@ def determine_fire(fireiter, i):
    	r = random.random()
    	if r > 0.5: return True
    	else: return False
-   else:
-	s = [int(j) for j in fireiter[1:-1].split(',')] 
-	if i in s: return True 
+   elif type(fireiter) is list:
+	if i in fireiter: return True 
 	else: return False
+   else: error("Error in configuration \"fire iteration\", input must integer array or \"random\"")
 
 def process_user(config):
    firelist = []
@@ -60,15 +81,21 @@ def process_user(config):
    	 fired, out = process_word(config, i)
    	#elif config['type'] is 'mouse':
    	# fired, out = process_mouse(config, i)
-	else:
-	 print "Error in configuration type, input must be \"picture/word/mouse\""
-	 exit()
-#   	show_picture(config['ISI'], config['ISI duration'])
+	else: error("Error in configuration \"type\", input must be \"picture/word/mouse\"")
+
+	if os.path.isexists(config['ISI image']): image_file = config['ISI image']
+	else: error("Error in configuration \"ISI image\", input must be valid image file\"")
+### DO display ISI image
+#	pISI = subprocess.Popen(["display", image_file])
+#	time.sleep(float(config['ISI duration'])/1000)
+#	pISI.terminate()
+#	pISI.kill()
+
    	if fired is True: firelist.append(i)
 	outlist.append(out)	
 
    config['fire iteration'] = firelist
-   config['out list'] = outlist
+   config['order list'] = outlist
    return config
 
 def process_config(filename):
@@ -78,7 +105,14 @@ def process_config(filename):
    	for row in reader:
    		k, x, v = row
    		config[k] = v
+
+   # Some error checking and cleaning of the config file
    config.pop('Name')
+   if config['fire iteration'] != "random":
+	s = config.pop('fire iteration')
+	fireiter = [int(i) for i in s[1:-1].split(',')] 
+	config['fire iteration'] = fireiter
+
    return config	
 
 def input_user_data():
