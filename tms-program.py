cddf7bcdc1cd0766020c38b06a8904283dc9c266
eugsokolov <eugsokolov@gmail.com>
2016-05-31 19:37:21 -0400

    fix tabs

--- tms-program.py
+++ tms-program.py
@@ -14,63 +14,63 @@ plt.show(block=False)
 objList = []
 
 def error(message):
-	print message
-	exit()
+     print(message)
+     exit()
 
 #Good reference for matplotlib
 #http://matplotlib.org/api/pyplot_summary.html
 def show_image(obj, screen, typeOut):
-    print typeOut, screen, obj
+    print(typeOut, screen, obj)
     plt.clf()
     plt.axis('off')
     if typeOut == 'image':
-	img = mpimg.imread(obj)
-	if screen == 'single':
-	    plt.imshow(img)
-	elif screen == 'double':
-	    plt.gcf().add_subplot(121)
-	    plt.axis('off')
-	    plt.imshow(img)
-	    plt.gcf().add_subplot(122)
-	    plt.axis('off')
-	    plt.imshow(img)
-	else: error('Error with screen type, must be single or double')
-	plt.draw()
+      img = mpimg.imread(obj)
+      if screen == 'single':
+         plt.imshow(img)
+      elif screen == 'double':
+         plt.gcf().add_subplot(121)
+         plt.axis('off')
+         plt.imshow(img)
+         plt.gcf().add_subplot(122)
+         plt.axis('off')
+         plt.imshow(img)
+      else: error('Error with screen type, must be single or double')
+      plt.draw()
     elif typeOut == 'text':
-	if screen == 'single':
-	    ax = plt.gcf().add_subplot(111)
-	    ax.text(4, 6, obj, fontsize=100)
-	    plt.axis([0,10,0,10])
-	elif screen == 'double':
-	    ax1 = plt.gcf().add_subplot(121)
-	    plt.axis('off')
-	    ax1.text(3, 5, obj, fontsize=100)
-	    plt.axis([0,10,0,10])
-	    ax2 = plt.gcf().add_subplot(122)
-	    plt.axis('off')
-	    ax2.text(4, 5, obj, fontsize=100)
-	    plt.axis([0,10,0,10])
-	else: error('Error with screen type, must be single or double')
-	plt.draw()
+      if screen == 'single':
+         ax = plt.gcf().add_subplot(111)
+         ax.text(4, 6, obj, fontsize=100)
+         plt.axis([0,10,0,10])
+      elif screen == 'double':
+         ax1 = plt.gcf().add_subplot(121)
+         plt.axis('off')
+         ax1.text(3, 5, obj, fontsize=100)
+         plt.axis([0,10,0,10])
+         ax2 = plt.gcf().add_subplot(122)
+         plt.axis('off')
+         ax2.text(4, 5, obj, fontsize=100)
+         plt.axis([0,10,0,10])
+      else: error('Error with screen type, must be single or double')
+      plt.draw()
     else: error('Error with type, must be image or text')
 
 #Fire TMS via Arduino as located by port Arduino is on
 #To be used with tms.ino
 def fire_tms(port):
-	print "FIRING TMS : ", port
-	#arduino = serial.Serial(port, 230400)
-	#arduino.write('1')
-	#arduino.close()
+     print("FIRING TMS : " + str(port))
+     #arduino = serial.Serial(port, 230400)
+     #arduino.write('1')
+     #arduino.close()
 
 #Determine if TMS should be triggered, given input array or randomization
 def determine_fire(fireiter, i):
    if fireiter == 'random':
-   	r = random.random()
-   	if r > 0.5: return True
-   	else: return False
+     r = random.random()
+     if r > 0.5: return True
+     else: return False
    elif type(fireiter) is list:
-	if i in fireiter: return True 
-	else: return False
+     if i in fireiter: return True 
+     else: return False
    else: error("Error in configuration \"fire iteration\", input must integer array or \"random\"")
 
 #Yield the next random object from global list objList
@@ -89,27 +89,27 @@ def process_type(config, i):
 
    #Process Image/Word with firing TMS
    if config['TMS before or after'] == "before":
-	if fire is True: fire_tms(config['TMS port'])
-	time.sleep(float(config['time to fire'])/1000)
-	show_image(out, screen, typeOut)
+     if fire is True: fire_tms(config['TMS port'])
+     time.sleep(float(config['time to fire'])/1000)
+     show_image(out, screen, typeOut)
    elif config['TMS before or after'] == "after":
-	show_image(out, screen, typeOut)
-	time.sleep(float(config['time to fire'])/1000)
-	if fire is True: fire_tms(config['TMS port'])
+     show_image(out, screen, typeOut)
+     time.sleep(float(config['time to fire'])/1000)
+     if fire is True: fire_tms(config['TMS port'])
    else: error("Error in configuration \"TMS before or after\", input must be \"before/after\"")
 
    #Process how to end event
    if config['event end'] == "keypress":
-	raw_input("waiting for keypress to end...")
+     raw_input("waiting for keypress to end...")
    elif config['event end'] == "time":
-	time.sleep(float(config['event end time'])/1000)
+     time.sleep(float(config['event end time'])/1000)
    else: error("Error in configuration \"event end\", input must be \"keypress/time\"")
 
    #Process refresh
    if config['refresh'] == "yes":
-	pass
+     pass
    elif config['refresh'] == "no":
-	pass 
+     pass 
 #TODO
    else: error("Error in configuration \"refresh\", input must be \"yes/no\"")
 
@@ -117,31 +117,31 @@ def process_type(config, i):
 
 def process_mouse(config, i):
 #TODO
-	return 1
+     return 1
 
 #Process the user information
 def process_user(config):
    firelist = []
    outlist = []
    for i in range(1, int(config['iterations per user'])+1):
-	print i
-   	if (config['type'] == 'image') or (config['type'] == 'text'):
-   	 fired, out = process_type(config, i)
-   	elif (config['type'] == 'mouse'):
-   	 error("mouse not yet implemented")
-	 fired, out = process_mouse(config, i)
-	else: error("Error in configuration \"type\", input must be \"image/text/mouse\"")
+     print(i)
+     if (config['type'] == 'image') or (config['type'] == 'text'):
+         fired, out = process_type(config, i)
+     elif (config['type'] == 'mouse'):
+         error("mouse not yet implemented")
+         fired, out = process_mouse(config, i)
+     else: error("Error in configuration \"type\", input must be \"image/text/mouse\"")
 
-	if config['ISI step'] != 'none':
-	   show_image(config['ISI step'], config['screen'], 'image')
-	   time.sleep(float(config['ISI step duration'])/1000)	
+     if config['ISI step'] != 'none':
+        show_image(config['ISI step'], config['screen'], 'image')
+        time.sleep(float(config['ISI step duration'])/1000)     
 
-   	if fired is True: firelist.append(i)
-	outlist.append(out)	
+     if fired is True: firelist.append(i)
+     outlist.append(out)     
 
    if config['ISI end'] != 'none':
-	show_image(config['ISI end'], config['screen'], 'image')
-	time.sleep(float(config['ISI end duration'])/1000)	
+     show_image(config['ISI end'], config['screen'], 'image')
+     time.sleep(float(config['ISI end duration'])/1000)     
 
    config['fire iteration'] = firelist
    config['order list'] = outlist
@@ -151,37 +151,37 @@ def process_user(config):
 def processYieldField(typeIn, directory):
     global objList
     if os.path.isfile(directory) and typeIn == 'text':
-	words = list()
-	with open(directory, 'r') as f:
-	 for line in f:
-	   cand = line.rsplit()[0]
-	   if type(cand) != str : error("Error in text list file")
-   	   words.append(cand)
-    	objList = words
+      words = list()
+      with open(directory, 'r') as f:
+      for line in f:
+        cand = line.rsplit()[0]
+        if type(cand) != str : error("Error in text list file")
+           words.append(cand)
+        objList = words
     elif os.path.isdir(directory) and typeIn == 'image':
-	fileNames = list()
-	path, dirs, files = os.walk(directory).next()
-	for f in files:
-		fileNames.append(path+"/"+f)
-	objList = fileNames
+      fileNames = list()
+      path, dirs, files = os.walk(directory).next()
+      for f in files:
+          fileNames.append(path+"/"+f)
+      objList = fileNames
     else: error("Error in configuration \"type\" or \"directory\"")
 
 def process_config(filename):
    config = {}
    with open(filename, 'r') as f:
-   	reader = csv.reader(f)
-   	for row in reader:
-   		k, x, v = row
-   		config[k] = v
+     reader = csv.reader(f)
+     for row in reader:
+          k, x, v = row
+          config[k] = v
    processYieldField(config['type'], config['directory'])
    # Some error checking and cleaning of the config file
    config.pop('Name')
    if config['fire iteration'] != "random":
-	s = config.pop('fire iteration')
-	fireiter = [int(i) for i in s[1:-1].split(',')] 
-	config['fire iteration'] = fireiter
+     s = config.pop('fire iteration')
+     fireiter = [int(i) for i in s[1:-1].split(',')] 
+     config['fire iteration'] = fireiter
 
-   return config	
+   return config     
 
 def input_user_data(name, sex):
    d = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
@@ -194,8 +194,8 @@ def input_user_data(name, sex):
 
 def log_user_data(info, filename):
    with open(filename, 'a') as f:
-	for k,v in sorted(info.items()):
-	   f.write(str(k) + ',' + str(v) + '\n')
+     for k,v in sorted(info.items()):
+        f.write(str(k) + ',' + str(v) + '\n')
    f.close()
 
 def start():
