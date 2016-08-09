de24fcb0541dab0ed434c7e6fed76259411f34b6
Thomas Koch <tomkoch96@yahoo.com>
2016-08-09 16:31:06 -0400

    Added logging, form data, some bug fixes

--- js/main.js
+++ js/main.js
@@ -22,27 +22,31 @@ const default_word = 'yes or no';
 const num_iter = 4; // number of total iterations
 const num_pics = 3; // 3 taylors
 const time_between_frames = 1000; // milisec between each image
-var i = 0; // iteration number
+var ii = 0; // iteration number
 var j = 0; // image number
 var k = 0; // responses 
 var responses = new Array(num_iter * num_pics);
 
-$('.button').click(function() {
-	$(window).data('response_recorded',false);
-	$(window).data('waiting',false); 
-    $('.button').fadeOut('fast');
+// TODO input from user (see index.html)
+var user = 'test';
+var port = 'COM1';
+var wordList = {};
+var text_output = "";
+
+$('form').submit(function(e) {
+	e.preventDefault(); //dont try and submit the form
+
+	user = $('#name').val();
+	port = $('#port').val();
+
+	$(window).data(
+		{'response_recorded': false,
+		'waiting': false
+	});
+
+    $('form').fadeOut('fast');
 	show_first_image();
 });
-
-// TODO input from user (see index.html)
-const user = 'test';
-const port = 'COM1';
-const wordList = {};
-wordList['one'] = 'easy';
-wordList['two'] = 'hard';
-wordList['three'] = 'med';
-wordList['four'] = 'easy';
-wordList['five'] = 'med';
 
 function get_random_word() {
     var keys = Object.keys(wordList);
@@ -61,7 +65,7 @@ function get_image(type, difficulty) {
             return images[2][type];
             break;
         default:
-            exit(1);
+            alert(type + difficulty);
     }
 }
 
@@ -75,10 +79,15 @@ function show_first_image() {
     var word = get_random_word();
 	$('.word').html(word); 
 	$('.picture').attr('src', get_image(j, wordList[word]));
+	$(window).data({
+		'current_word': word,
+		'current_difficulty': wordList[word]
+	});
 	j++;
 	setTimeout(function() { //let user absorb word			
         fire_tms(port);
 		show_next_image(wordList[word]);
+		return;
 	}, time_between_frames);
 }
 
@@ -88,17 +97,27 @@ function show_next_image(difficulty) {
 		show_last_image();
 		return;
 	}
-	if(i == num_iter) { 
+	if(ii == num_iter) { 
         log_data();
 		return; //end the program
 	}
 
 	$('.word').html(default_word);
 	$('.picture').attr('src',get_image(j, difficulty));
-	$(window).data('waiting',true); //get input
-	$(window).data('response_recorded',false);
+	d = new Date();
+	n = d.getTime();
+	$(window).data({
+		'waiting': true,
+		'response_recorded': false,
+		'current_response_frame': j,
+		'waiting_response_start_time': n
+	}); //get input
+
 	setTimeout(function() { //get response (if any)
-		if($(window).data('response_recorded')) responses[k] = $(window).data('response');
+		if($(window).data('response_recorded')) {
+		 	responses[k] = $(window).data('response');
+		 	add_to_log_data();
+		}
 		else responses[k] = null;
 		k++;
 		j++;
@@ -111,44 +130,94 @@ function show_last_image() {
 	$('.word').html('');
 	setTimeout(function() {
 		j = 0;
-		i++;
+		ii++;
 		show_first_image();
 		return;
 	}, time_between_frames);
 }
 
-// TODO log to file: logFile = log/name+time.log
-// iteration objects:
-// {word, difficulty, response, responseFrame, responseTime}
-// (responseFrame is the element j)
+function add_to_log_data() {
+	word = $(window).data('current_word');
+	difficulty = $(window).data('current_difficulty');
+	response = $(window).data('response');
+	responseFrame = $(window).data('current_response_frame');
+	responseTime = $(window).data('waiting_response_end_time') - $(window).data('waiting_response_start_time');
+
+	text_output += word + "," + difficulty + "," + response + ","
+		+ responseFrame + "," + responseTime + "\n";
+}
+
 function log_data() {
 	$('.picture').attr('src', 'images/end.jpg');
 	$('.word').html('FIN');
-    for(i = 0; i < responses.length;i ++) {
-		if(responses[i] != null)
-			html = "<li>" + responses[i] + "</li>";
-		else html = "<li>No Response</li>";
-		$('.responses').append(html); //print it for yourself
-	}
+
+	d = new Date();
+	time = d.toDateString();
+
+	file_name = "log/" + user + time + ".log"; //saves to Downloads folder
+
+    var blob = new Blob([text_output], {type: "text/plain;charset=utf-8"});
+  	saveAs(blob, file_name);
 }
 
-// TODO record response time (the time to response to the yes or no)
 $(window).on('keypress',function(e) { //catches response
 	key = e.key;
 	//only get response if we are waiting for one and 
     //have not already recorded one
 	if($(window).data('waiting') && !$(window).data('response_recorded')) {
+		d = new Date();
+		n = d.getTime();
 		switch(e.key) {
 			case 'y':
-				$(window).data('response_recorded',true);
-				$(window).data('response','y');
+				$(window).data({
+					'response_recorded': true,
+					'response': 'y',
+					'waiting_response_end_time': n
+				});
 				break;
 			case 'n':
-				$(window).data('response_recorded',true);
-				$(window).data('response','n');
+				$(window).data({
+					'response_recorded': true,
+					'response': 'n',
+					'waiting_response_end_time': n
+				});
 				break;
 			default:
 				break;
 		}
 	}
 });
+
+$('#file_input').on('change',function(ev) {
+    var f = ev.target.files[0]; 
+
+    if (f) {
+      var r = new FileReader();
+      r.readAsText(f);
+      r.onloadend = function(e) { 
+	    var contents = e.target.result;
+      	lines = processData(contents);
+      	for(i = 1; i < lines.length; i++) {
+      		tmp = lines[i][0];
+      		tmp = tmp.split(',');
+      		wordList[i-1] = tmp[1]; //populate word list
+      	}
+      }
+    } else { 
+      alert("Failed to load file");
+    }
+});
+
+function processData(csv) {
+    var allTextLines = csv.split(/\r\n|\n/);
+    var lines = [];
+    for (i=0; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(';');
+            var tarr = [];
+            for (j=0; j<data.length; j++) {
+                tarr.push(data[j]);
+            }
+            lines.push(tarr);
+    }
+  	return lines;
+}
